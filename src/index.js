c3dfd44e54afed0f3dc0d79980f1ce74cc85b5d6
Zyryanova Anastasia <anastasia.zyryanova.97@gmail.com>
2021-11-18 17:49:46 +0500

    задание 1-5 без стилей

--- src/index.js
+++ src/index.js
@@ -1,47 +1,106 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-// const films = [{title: 'film1', year: 2020}, {title: 'film2', year: 2020}]
-// реакт элемент (дерево)
-// const ReactElm = (
-//  <div>
-//    <h1>ReactElm</h1>
-//    <div>
-//      {films.map(({title, year}) => (
-//        // нельзя вернуть список элементов, нужно оборачивать в 1 родительский
-//        <div>
-//          <div>title:{title}</div>
-//          <div>title:{year}</div>
-//        </div>
-//      ))}
-//    </div>
-//  </div>
-// );
- 
-// const FunctionComponent = () => {
-//  return ReactElm;
-// }
-
-const Message = ({messageText}) => {
-  return (
-    <div>{messageText}</div>
-  )
-};
-
-const App = ({...rest}) => {
+const EmptyList = () => {
   return (
     <div>
-      <Message {...rest}/>
+      <h3>Чат пустой</h3>
+    </div>
+  );
+};
+
+const MessageList = ({ messageList }) => {
+  return (
+    <div>
+      {messageList.map(({ text, author, date, time }) => (
+        <div>
+          <div>author: {author}</div>
+          <div>date: {date}</div>
+          <div>time: {time}</div>
+          <div>text: {text}</div>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [newMessageText, setNewMessageText] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const getDate = () => {
+    return new Date().toLocaleDateString();
+  };
+
+  const getTime = () => {
+    return new Date().toLocaleTimeString().slice(0, -3);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (
+        messageList.length !== 0 &&
+        messageList[messageList.length - 1].author !== "Сhat-bot"
+      ) {
+        setMessageList([
+          ...messageList,
+          {
+            author: "Сhat-bot",
+            text: "Ваше сообщение отправлено",
+            getDate,
+            getTime,
+          },
+        ]);
+      }
+    }, 2000);
+  }, [messageList]);
+
+  const addMessage = () => {
+    var btn = document.getElementById("sendMessage");
+    btn.disabled = true;
+    window.setTimeout(() => {
+      btn.disabled = false;
+    }, 2500);
+    setMessageList([
+      ...messageList,
+      {
+        author: "Anonymous",
+        text: newMessageText,
+        date: getDate(),
+        time: getTime(),
+      },
+    ]);
+    setNewMessageText("");
+  };
+
+  const handleChangeValue = (event) => {
+    setNewMessageText(event.target.value);
+  };
+
+  return (
+    <div>
+      {Object.keys(messageList).length !== 0 ? (
+        <MessageList messageList={messageList} />
+      ) : (
+        <EmptyList />
+      )}
+      <input
+        placeholder="Введите сообщение"
+        onChange={handleChangeValue}
+        value={newMessageText}
+      />
+      <button onClick={addMessage} id="sendMessage">
+        add new message
+      </button>
     </div>
   );
 };
 
 ReactDOM.render(
   <React.StrictMode>
-     <App 
-     messageText="Текст сообщения"
-     />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
